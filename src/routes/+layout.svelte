b92715d3c51a0279bb934b5aa9d1f8416db35628
Ryuusei1017 <126587411+Ryuusei1017@users.noreply.github.com>
2024-09-28 08:38:56 +0900

    new

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,3 +11,4 @@
         <slot />
     </main>
 </div>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,3 +24,49 @@
 {:else}
     <p>まだ投票ボックスがない。</p>
 {/if}
+
+<div>
+    <main class="border border-black rounded-md p-4 mx-60 my-20 justify-center">
+        <h1 class="text-6xl text-center">まどまぎ かわいいキャラランキング</h1>
+        <div class="flex ">
+            <img src="src/800.jpg" alt="まどまぎのイラスト" class="w-1/2 p-6">  
+            <ul class="pl-4 text-4xl space-y-7 ranking flex flex-col justify-center">
+                <li>暁美ほむら</li>
+                <li>佐倉杏子</li>
+                <li>巴マミ</li>
+                <li>鹿目まどか</li>
+                <li>美樹さやか</li>
+            </ul>
+        </div>
+    </main>
+</div>
+
+<div>
+    <main class="border border-black rounded-md p-4 mx-60 my-20 justify-center">
+        <h1 class="text-6xl text-center">ゼンゼロ 強いキャラランキング</h1>
+        <div class="flex ">
+            <img src="src\FXI2TJ_UUAAsArP.jpg" alt="まどまぎのイラスト" class="w-1/2 p-6">  
+            <ul class="pl-4 text-4xl space-y-7 ranking flex flex-col justify-center">
+                <li>猫又</li>
+                <li>エレンジョー</li>
+                <li>青衣</li>
+                <li>アンビー</li>
+                <li>ニコ</li>
+            </ul>
+        </div>
+    </main>
+</div>
+
+<style>
+    
+.ranking {
+  counter-reset: rank;
+
+}
+ 
+.ranking li::before {
+  counter-increment: rank;
+  content: counter(rank) "位";
+  padding-right: 10px;
+}
+</style>
